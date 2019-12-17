b5990853e631f26a221e8f4545b046546e5e43a1
Maxime Charlebois <mnmpdadish@hotmail.com>
2019-12-17 18:36:31 -0500

    new 12x12 example

---
+++ samples/spectrum/12x12_U8_Ne126/run_vmc.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#QSUB -queue F144cpu
+#QSUB -node 144
+#QSUB -mpi 144
+#QSUB -omp 24
+#QSUB -place pack
+#QSUB -over false
+#PBS -N run_vmc.sh
+#PBS -l walltime=23:55:00
+cd ${PBS_O_WORKDIR}
+
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=24
+
+#init_groundstate.py opt.in
+#mkdir output
+#mv zqp_opt.dat output/.
+
+#### mVMC groud state calculations:
+date
+mpijob vmc_new.out namelist.def ./output/zqp_opt.dat
+date
+mpijob vmc_new.out namelist_G.def ./output/zqp_opt.dat
+date
+
