correcting averaging bug

tool/dvmc/mergeOutputBin.py
@@ -28,13 +28,14 @@ from ctypes import cdll, c_int, c_float, pointer, c_char_p
 full_path = os.path.realpath(__file__)
 pythonPathCode, file1 = os.path.split(full_path)
 
+print(pythonPathCode)
 
 L=W=1
 Excitation =  open('excitation.def').read().replace(' ','')
 n_exc = int(re.compile('NExcitation([0-9]*)').findall(Excitation)[0])
 L = int(re.compile('L([0-9]*)').findall(Excitation)[0])
 W = int(re.compile('W([0-9]*)').findall(Excitation)[0])
-print n_exc, L, W
+print(n_exc, L, W)
 
 def X(ri):
   return ri%W
@@ -48,22 +49,22 @@ def main():
   fileIn = []
   n_file = len(sys.argv[:])-1
   n_file2 = 0
-  print sys.argv[:]
-  print n_file
+  print(sys.argv[:])
+  print(n_file)
   
   for nn in range(0,n_file):
     n_file2 +=1
     fileIn.append(sys.argv[1+nn])
   
-  print fileIn
+  print(fileIn)
   if (n_file <1):
-    print 'error: no input files.\nexample\n$ mergeOutputBin.py output/zvo_nCHAm_nAHCm_00*\n'
+    print('error: no input files.\nexample\n$ mergeOutputBin.py output/zvo_nCHAm_nAHCm_00*\n')
     exit()
     
   Nsite = L*W
   #use compiled version if present:
   if( not os.path.isfile(pythonPathCode+'/libdvmc_speedup.so')):
-    print 'Error: the shared object library "libdvmc_speedup.so" was not found.\nTo obtain it, go in the original directory of this python code (probably: '+ pythonPathCode+'/) and do make.'
+    print('Error: the shared object library "libdvmc_speedup.so" was not found.\nTo obtain it, go in the original directory of this python code (probably: '+ pythonPathCode+'/) and do make.')
     exit(0)
   c_ArrayFloatN = c_float * (n_exc*n_exc*Nsite*Nsite)
 
@@ -77,14 +78,17 @@ def main():
   c_ncomp = c_int(n_exc*n_exc*Nsite*4)
 
   argList = (c_char_p * len(fileIn))()
-  argList[:] = fileIn
-  
+  #argList[:] = fileIn
+  for i in range(len(fileIn)):
+    fileIn[i]=fileIn[i].encode('utf-8')
+  argList[:] = fileIn  
+
   #Loading and using our home made module:
   lib1 = cdll.LoadLibrary(pythonPathCode+'/libdvmc_speedup.so')
   lib1.mergeOutputBin(len(fileIn), argList, c_NExcitation, c_L, c_W, 
               phys_CA_averaged,phys_AC_averaged,phys_CHA_averaged,phys_AHC_averaged, 1)
   
-  print 'after read'
+  print('after read')
   def convert_c2numpy(phys_averaged):
     numpy_averaged = np.zeros(n_exc*n_exc*Nsite*Nsite)
     numpy_averaged[:] = phys_averaged[:]
@@ -103,14 +107,14 @@ def main():
   nCHAm_up = convert_c2numpy(phys_CHA_averaged)
   nAHCm_up = convert_c2numpy(phys_AHC_averaged)
 
-  print nCAm_up
+  print(nCAm_up)
 
-  print 'after reshape'
+  print('after reshape')
     
-  print dirOutput+'S_CA'
-  print dirOutput+'S_AC'
-  print dirOutput+'S_CHA'
-  print dirOutput+'S_AHC'
+  print(dirOutput+'S_CA')
+  print(dirOutput+'S_AC')
+  print(dirOutput+'S_CHA')
+  print(dirOutput+'S_AHC')
   np.save(dirOutput+'S_CA',nCAm_up)
   np.save(dirOutput+'S_AC',nACm_up)
   np.save(dirOutput+'S_CHA',nCHAm_up)